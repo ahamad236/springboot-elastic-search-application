40544ba255b4e15b86860b755683740122172847
Ahamad <ahmadshaik4u@gmail.com>
2025-11-07 00:41:19 +0530

    upto step 5 done

---
+++ src/test/java/com/ahamadjal/springboot_elasticsearch_application/SpringbootElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahamadjal.springboot_elasticsearch_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
